84ca1e05b11126a7217bda8a42c799f670705365
Priyanshu Pandey <89788989+priyanshupp04@users.noreply.github.com>
2022-03-26 06:14:56 +0000

    Create tcp.java

---
+++ tcp.java
@@ -0,0 +1,60 @@
+import java.net.Socket;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+class Client {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 1300);
+            Scanner socketScanner = new Scanner(socket.getInputStream());
+            Scanner consoleScanner = new Scanner(System.in);
+            System.out.println("Enter File Name");
+            String fileName = consoleScanner.nextLine();
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            printStream.println(fileName);
+            while (socketScanner.hasNextLine()) {
+                System.out.println(socketScanner.nextLine());
+            }
+            socket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1300);
+            Socket socket = serverSocket.accept();
+            Scanner socketScanner = new Scanner(socket.getInputStream());
+            String fileName = socketScanner.nextLine().trim();
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            File file = new File(fileName);
+            if (file.exists()) {
+                Scanner fileScanner = new Scanner(file);
+                while (fileScanner.hasNextLine()) {
+                    printStream.println(fileScanner.nextLine());
+                }
+            } else {
+                printStream.println("ERROR: FILE DOESN'T EXISTS");
+            }
+            // Keep the server alive for complete transfer of data
+            System.in.read();
+
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+
+        }
+    }
+}
